23fb8408d692b0346ea71414fd7e40e1421796c5
Jason Stajich <jasonstajich.phd@gmail.com>
2024-09-04 14:49:26 -0700

    mapping reads

--- v1/pipeline/03_map_reads_to_catalog.sh
+++ v1/pipeline/03_map_reads_to_catalog.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash -l
-#SBATCH --mem 16gb -c 48 -N 1 -n 1 --out logs/map_reads_to_catalog.%a.log
+#SBATCH --mem 96gb -c 48 -N 1 -n 1 --out logs/map_reads_to_catalog_95.%a.log
 
 module load workspace/scratch
 module load bwa-mem2
@@ -7,8 +7,6 @@ module load samtools
 
 INPUT=input_cds
 IDENTITY=95
-AACONSENSUS=db/LsFMGC_AA_${IDENTITY}_rep.fasta # or whatever name you chose, and indicating perhaps what clustering method you used
-CDSDB=db/LsFMGC_CDS_all.fasta.gz 
 DB=db/LsFMGC_AA_${IDENTITY}_rep.to_CDS.fasta
 SAMPFILE=wood_frog_samples.csv
 WORK=working
@@ -16,20 +14,15 @@ OUT=results/mapping
 TEMP=$SCRATCH
 mkdir -p $OUT
 
-if [ ! -s $DB ]; then
-    grep '^>' $AACONSENSUS | sed 's/>//' > $SCRATCH/aa_names.txt
-    samtools faidx $CDSDB -r $SCRATCH/aa_names.txt > $DB
-fi
-
-# index the db for bwa mapping
-if [[ ! -f $DB.pac ]]; then
-    bwa-mem2 index $DB
-fi
-
-
 CPU=2
 if [ $SLURM_CPUS_ON_NODE ]; then
     CPU=$SLURM_CPUS_ON_NODE
+    SORTTHREAD=8
+    CPU=$(expr $CPU - $SORTTHREAD)
+    if $CPU -lt 1; then
+        CPU=$SLURM_CPUS_ON_NODE
+        SORTTHREAD=1
+    fi
 fi
 N=${SLURM_ARRAY_TASK_ID}
 if [ -z $N ]; then
@@ -44,7 +37,8 @@ tail -n +2 $SAMPFILE | sed -n ${N}p | while read STRAIN SHOTGUN
 do  
     LEFT=$WORK/$STRAIN/${STRAIN}_R1.fq.gz
     RIGHT=$WORK/$STRAIN/${STRAIN}_R2.fq.gz
-    SRTED=$OUT/$STRAIN.cram
+    SRTED=$OUT/$STRAIN.${IDENTITY}_rep.bam
     READGROUP="@RG\tID:$STRAIN\tSM:$STRAIN\tLB:$STRAIN\tPL:illumina\tCN:UHM"
-    bwa-mem2 mem -t $CPU -o $DB $LEFT $RIGHT | samtools sort --threads 8 -O bam -o $SRTED -T $TEMP -
+    bwa-mem2 mem -Y -t $CPU $DB $LEFT $RIGHT | samtools sort --threads $SORTTHREAD -O bam -o $SRTED -T $TEMP -
+    samtools index $SRTED
 done
--- v1/pipeline/04_quantify_gene_abundance.sh
+++ v1/pipeline/04_quantify_gene_abundance.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash -l
-#SBATCH -p short --mem 64gb -c 16 -N 1 -n 1 --out logs/quantify_gene_abundance.log
+#SBATCH -p short --mem 384gb -c 64 -N 1 -n 1 --out logs/quantify_gene_abundance_95.log
 
 module load subread
 
@@ -9,14 +9,19 @@ if [ $SLURM_CPUS_ON_NODE ]; then
 fi
 
 # we need to construct a GTF file from the consensus mmseqs/cd-hit fasta CDS
-GTF=$(echo -n $DB | perl -p -e 's/\.fasta/.gtf/')
+
 OUTDIR=results/quantify
 MAPPING=results/mapping
+IDENTITY=95
+GENOME=db/LsFMGC_AA_${IDENTITY}_rep.to_CDS.fasta
+# this file was make in step 02 
+GTF=$(echo -n $GENOME | perl -p -e 's/\.fasta/.gtf/')
+
 mkdir -p $OUTDIR
-GENOME=db/LsFMGC_cluster_rep.fasta
-if [[ ! -f $DB.gtf ]]; then
-    python scripts/fasta_to_gtf.py $DB > $GTF
-fi
+
 if [ ! -s $OUTDIR/LsFMGC_coverage.tsv ]; then
-    featureCounts -p  -o $OUTDIR/LsFMGC_coverage.tsv -G $GENOME -J -g gene_id -F GTF --countReadPairs $(find $MAPPING -size +0 -name "*.bam")
+    # not really paired end perhaps because the CDS are too short anyways
+    featureCounts -p -o $OUTDIR/LsFMGC_coverage_${IDENTITY}.tsv -a $GTF -O -M \
+        -G $GENOME -g gene_id -F GTF -t ORF --tmpDir $SCRATCH  --fraction \
+        --countReadPairs $(find $MAPPING -size +0 -name "*.${IDENTITY}_rep.bam")
 fi
